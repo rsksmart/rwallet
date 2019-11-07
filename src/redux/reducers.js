8c3b3b6ea6f4e491f195d426b7d517873eb1933d
chrisli30 <chrisli30@users.noreply.github.com>
2019-11-06 20:28:53 -0800

    Added root saga and reducers under src/redux

---
+++ src/redux/reducers.js
@@ -0,0 +1,7 @@
+import App from './app/reducer';
+import LanguageSwitcher from './languageSwitcher/reducer';
+
+export default {
+  App,
+  LanguageSwitcher,
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,28 @@
-import {createStore, applyMiddleware, StoreCreator} from 'redux'
-import rootReducer from "./reducers/reducers";
-import thunkMiddleware from 'redux-thunk'
-import { createLogger } from 'redux-logger'
+import {
+  createStore, combineReducers, applyMiddleware, compose,
+} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { routerReducer } from 'react-router-redux';
+import reducers from './reducers';
+import rootSaga from './sagas';
 
+// Define all middle wares
 const loggerMiddleware = createLogger();
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [thunkMiddleware, sagaMiddleware, loggerMiddleware];
 
+// Create Store object
 const store = createStore(
-    rootReducer,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    )
+  combineReducers({
+    ...reducers,
+    router: routerReducer,
+  }),
+  compose(applyMiddleware(...middlewares)),
 );
+
+// Start Saga middle ware
+sagaMiddleware.run(rootSaga);
 
 export default store;
