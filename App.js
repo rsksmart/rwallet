7307a81360b091134720dd56e3546b3111c31e94
imstar15 <imstar15@qq.com>
2019-11-13 17:07:18 +0800

    All pages. Remove parse in App.js

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import './shim';
 import React from 'react';
-import AsyncStorage from '@react-native-community/async-storage';
 import { Provider } from 'react-redux';
-import Parse from 'parse/react-native';
 import store from './src/redux/store';
 import RootSwitchNavigator from './src/navigation/container';
-import config from './config';
 import appContext from './src/common/appContext';
 import walletManager from './src/common/wallet/walletManager';
 
@@ -16,10 +13,5 @@ export default function App() {
     </Provider>
   );
 }
-
-Parse.initialize(config.appId, config.javascriptKey, config.masterKey);
-Parse.serverURL = config.serverURL;
-Parse.masterKey = config.masterKey;
-Parse.setAsyncStorage(AsyncStorage);
 
 appContext.init(walletManager);
