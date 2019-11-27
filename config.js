aef97ffe9645bfa739d0e57f6d4d3bac3af925c9
chrisli30 <chrisli30@users.noreply.github.com>
2019-11-26 17:58:15 -0800

    Moved currecy const setup to config.js

--- config.js
+++ config.js
@@ -7,6 +7,16 @@ const config = {
     masterKey: '5a269cfebfde46a9acec7b3273bf6c245a269cfebfde46a9acec7b3273bf6c24',
     serverURL: 'http://192.168.31.177:1338/parse',
   },
+  settings: {
+    currency: [
+      { name: 'USD', symbol: '$' },
+      { name: 'CNY', symbol: '￥' },
+      { name: 'ARS', symbol: 'ARS$' },
+      { name: 'KRW', symbol: '₩' },
+      { name: 'JPY', symbol: '￥' },
+      { name: 'GBP', symbol: '£' },
+    ],
+  },
 };
 
 export default config;
