29d928026b860f3824c022c233bfb7367524c23f
ericw <768616253@qq.com>
2019-11-30 11:11:05 +1300

    change for the requirement

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,8 +23,6 @@ module.exports = {
   rules: {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],"react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     'no-console': 'off',
-    'no-case-declarations': 'off',
-    'react/jsx-props-no-spreading': 'off',
     "max-len": ["error", { "code": 250, "ignoreComments": true }]
   },
   "parser": "babel-eslint",
