ea23c5958b36e1daa1a331833946fda87466bda3
imstar15 <imstar15@qq.com>
2019-12-20 00:47:48 +0800

    Fix wallet fetching interval

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const config = {
     appId: 'rwallet',
     javascriptKey: '',
     masterKey: '5a269cfebfde46a9acec7b3273bf6c245a269cfebfde46a9acec7b3273bf6c24',
-    serverURL: 'http://10.0.2.2:1338/parse',
+    serverURL: 'http://130.211.12.3/parse',
   },
   defaultSettings: {
     language: 'en',
@@ -31,9 +31,9 @@ const config = {
     reduxLoggerEnabled: false,
   },
   interval: {
-    fetchPrice: 30000,
-    fetchBalance: 30000,
-    fetchTransaction: 30000,
+    fetchPrice: 8000,
+    fetchBalance: 8000,
+    fetchTransaction: 8000,
   },
 };
 
