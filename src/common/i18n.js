7a05b34e28145993f20fe7a624a32434420b54cc
imstar15 <imstar15@qq.com>
2019-12-23 14:53:25 +0800

    Replace i18n.defaultSeparator

--- src/common/i18n.js
+++ src/common/i18n.js
@@ -7,6 +7,10 @@ import es from './translations/es.json';
 import pt from './translations/pt.json';
 import zh from './translations/zh.json';
 
+// Change the default separator
+// https://github.com/AlexanderZaytsev/react-native-i18n/issues/73
+I18n.defaultSeparator = '/';
+
 // Should the app fallback to English if user locale doesn't exists
 I18n.fallbacks = true;
 
@@ -23,18 +27,10 @@ export const isRTL = currentLocale.indexOf('he') === 0 || currentLocale.indexOf(
 // Allow RTL alignment in RTL languages
 ReactNative.I18nManager.allowRTL(isRTL);
 
-// The method we'll use instead of a regular string
-export function isContainTranslation(words) {
-  return en[words];
-}
-
 // If contain translation, return translation, else return original text.
 export function strings(name, params = {}) {
-  const isContain = isContainTranslation(name);
-  if (isContain) {
-    return I18n.t(name, params);
-  }
-  return name;
+  const translation = I18n.t(name, { ...params, defaultValue: name });
+  return translation;
 }
 
 
