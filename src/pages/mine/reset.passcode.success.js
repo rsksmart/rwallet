ef1e7ef7d89bdbe8600172dd106badc3c20d8e88
imstar15 <imstar15@qq.com>
2019-12-24 16:54:48 +0800

    Fix reset success icon position

--- src/pages/mine/reset.passcode.success.js
+++ src/pages/mine/reset.passcode.success.js
@@ -3,10 +3,9 @@ import {
   View, StyleSheet, Image,
 } from 'react-native';
 import PropTypes from 'prop-types';
-import { StackActions, NavigationActions } from 'react-navigation';
+import { StackActions } from 'react-navigation';
 import flex from '../../assets/styles/layout.flex';
 import Button from '../../components/common/button/button';
-import appContext from '../../common/appContext';
 import Loc from '../../components/common/misc/loc';
 import Header from '../../components/common/misc/header';
 import screenHelper from '../../common/screenHelper';
@@ -24,7 +23,7 @@ const styles = StyleSheet.create({
   },
   content: {
     alignItems: 'center',
-    marginTop: 200,
+    marginTop: 30,
   },
   check: {
     margin: 25,
@@ -41,15 +40,23 @@ export default class ResetPasscodeSuccess extends Component {
       header: null,
     });
 
-    render() {
+    constructor(props) {
+      super(props);
+      this.onBackPress = this.onBackPress.bind(this);
+    }
+
+    onBackPress() {
       const { navigation } = this.props;
+      const statckActions = StackActions.popToTop();
+      navigation.dispatch(statckActions);
+    }
+
+    render() {
       return (
         <View style={[flex.flex1]}>
           <Header
             title="Reset Passcode"
-            goBack={() => {
-              navigation.goBack();
-            }}
+            goBack={this.onBackPress}
           />
           <View style={screenHelper.styles.body}>
             <View style={styles.content}>
@@ -60,25 +67,7 @@ export default class ResetPasscodeSuccess extends Component {
           <View style={styles.buttonView}>
             <Button
               text="BACK TO SETTING"
-              onPress={async () => {
-                let page = null;
-                if (navigation.state.params) {
-                  page = navigation.state.params.page;
-                }
-                if (page && page === 'Transfer') {
-                  appContext.eventEmitter.emit('onFirstPasscode');
-                  navigation.navigate('Transfer');
-                } else {
-                  const resetAction = StackActions.reset({
-                    index: 1,
-                    actions: [
-                      NavigationActions.navigate({ routeName: 'MineIndex' }),
-                      NavigationActions.navigate({ routeName: 'TwoFactorAuth' }),
-                    ],
-                  });
-                  navigation.dispatch(resetAction);
-                }
-              }}
+              onPress={this.onBackPress}
             />
           </View>
         </View>
