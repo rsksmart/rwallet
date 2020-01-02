c071507694e95987950ce0e059c8b176f8317290
ericw <768616253@qq.com>
2020-01-03 00:08:15 +1300

    fix issue-251

--- src/pages/wallet/dashboard.js
+++ src/pages/wallet/dashboard.js
@@ -5,11 +5,19 @@ import { isEmpty } from 'lodash';
 import PropTypes from 'prop-types';
 import List from './list';
 import AddIndex from './add.index';
+import appActions from '../../redux/app/actions';
 
 class Dashboard extends Component {
     static navigationOptions = () => ({
       header: null,
     });
+
+    async componentDidMount() {
+      const { wallets, showPasscode } = this.props;
+      if (!isEmpty(wallets)) {
+        showPasscode('verify');
+      }
+    }
 
     render() {
       const { navigation, wallets } = this.props;
@@ -26,6 +34,7 @@ Dashboard.propTypes = {
     state: PropTypes.object.isRequired,
   }).isRequired,
   wallets: PropTypes.arrayOf(PropTypes.object),
+  showPasscode: PropTypes.func.isRequired,
 };
 
 Dashboard.defaultProps = {
@@ -35,6 +44,13 @@ Dashboard.defaultProps = {
 const mapStateToProps = (state) => ({
   isWalletsUpdated: state.Wallet.get('isWalletsUpdated'),
   wallets: state.Wallet.get('walletManager') && state.Wallet.get('walletManager').wallets,
+
 });
 
-export default connect(mapStateToProps, null)(Dashboard);
+const mapDispatchToProps = (dispatch) => ({
+  showPasscode: (category) => dispatch(
+    appActions.showPasscode(category),
+  ),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
