a6fb72612bfdef19e559e67a21b18e8e58681cde
imstar15 <imstar15@qq.com>
2020-01-23 12:27:21 +0800

    Modify comment

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import './shim'; // provide node types for packages
+import './shim'; // Provide global node types(global.Buffer, global.process) for node_modules packages(bitcoinjs-lib, bip32)
 import RootSwitchNavigator from './src/navigation/container';
 import store from './src/redux/store';
 import common from './src/common/common';
