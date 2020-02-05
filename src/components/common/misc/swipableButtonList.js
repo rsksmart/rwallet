544d3358c0aad947a8f4d2fff405584bafc2ca0e
imstar15 <imstar15@qq.com>
2020-02-05 14:17:38 +0800

    wallet list, history, transaction

--- src/components/common/misc/swipableButtonList.js
+++ src/components/common/misc/swipableButtonList.js
@@ -109,7 +109,7 @@ export default class SwipableButtonList extends Component {
               ]}
               onPress={() => this.listView.safeCloseOpenRow()}
             >
-              <Loc style={[styles.backText]} text="Swap" />
+              <Loc style={[styles.backText]} text="button.swap" />
             </TouchableOpacity>
             <TouchableOpacity
               style={[
@@ -123,7 +123,7 @@ export default class SwipableButtonList extends Component {
                 }
               }}
             >
-              <Loc style={[styles.backText]} text="Send" />
+              <Loc style={[styles.backText]} text="button.send" />
             </TouchableOpacity>
             <TouchableOpacity
               style={[
@@ -137,7 +137,7 @@ export default class SwipableButtonList extends Component {
                 }
               }}
             >
-              <Loc style={[styles.backText]} text="Receive" />
+              <Loc style={[styles.backText]} text="button.receive" />
             </TouchableOpacity>
           </View>
         )}
