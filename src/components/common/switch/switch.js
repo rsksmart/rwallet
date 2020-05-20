7d42631bc06f6ceecbcec2ad61aa0b3394d8e702
AmazingBeerBelly <amazingbeerbelly@gmail.com>
2020-05-20 17:17:02 +0800

    code review: 1. any params can be passed in custom Switch

--- src/components/common/switch/switch.js
+++ src/components/common/switch/switch.js
@@ -2,31 +2,14 @@ import React from 'react';
 import {
   Switch as RNSwitch, Platform,
 } from 'react-native';
-import PropTypes from 'prop-types';
 import color from '../../../assets/styles/color.ts';
 
-export default function Switch({
-  value, onValueChange, style, disabled,
-}) {
+export default function Switch(props) {
   return (
     <RNSwitch
       trackColor={Platform.OS === 'ios' ? { false: 'gray', true: color.app.theme } : {}}
-      value={value}
-      onValueChange={onValueChange}
-      style={style}
-      disabled={disabled}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
     />
   );
 }
-
-Switch.propTypes = {
-  value: PropTypes.bool.isRequired,
-  onValueChange: PropTypes.func.isRequired,
-  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
-  disabled: PropTypes.bool,
-};
-
-Switch.defaultProps = {
-  style: null,
-  disabled: false,
-};
