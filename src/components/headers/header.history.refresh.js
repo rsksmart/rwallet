800d95c6c8041d598cc95578083fcb0d24c9ed94
AmazingBeerBelly <amazingbeerbelly@gmail.com>
2020-05-27 13:49:41 +0800

    add comments with RefreshHeader's height

--- src/components/headers/header.history.refresh.js
+++ src/components/headers/header.history.refresh.js
@@ -3,6 +3,7 @@ import { ActivityIndicator } from 'react-native';
 import { RefreshHeader as Header } from 'react-native-spring-scrollview';
 
 export default class RefreshHeader extends Header {
+    // "height" is the react-native-spring-scrollview/RefreshHeader's static variable，overwrite it can control the refresh header's height
     static height = 50;
 
     render() {
