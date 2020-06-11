c1de29b7682dbe39d09b007a7f4c840e1e46c686
AmazingBeerBelly <amazingbeerbelly@gmail.com>
2020-06-11 11:36:51 +0800

    set provider to window.web3, overwrite metamask function

--- src/pages/dapp/browser.js
+++ src/pages/dapp/browser.js
@@ -9,27 +9,6 @@ import { connect } from 'react-redux';
 import OperationHeader from '../../components/headers/header.operation';
 import appActions from '../../redux/app/actions';
 
-const contractAddress = ethers.utils.getAddress(('0x248B320687eBf655f9eE7F62F0388c79fBB7b2F4').toLowerCase());
-const rskEndpoint = 'https://public-node.testnet.rsk.co';
-const abi = [
-  {
-    constant: false,
-    inputs: [
-      {
-        name: 'to',
-        type: 'address',
-      },
-    ],
-    name: 'dispense',
-    outputs: [],
-    payable: false,
-    stateMutability: 'nonpayable',
-    type: 'function',
-    signature: '0x5f746233',
-  },
-];
-
-
 class DAppBrowser extends Component {
   static navigationOptions = () => ({
     header: null,
@@ -37,7 +16,7 @@ class DAppBrowser extends Component {
 
   getJsCode = (address) => `
       (function() {
-        let resolver, rejecter, hash, id = 1, status = false
+        let resolver, rejecter, hash, status = false
         setTimeout(() => {
           ${Platform.OS === 'ios' ? 'window' : 'document'}.addEventListener("message", function(data) {
             const result = data.data
@@ -58,18 +37,20 @@ class DAppBrowser extends Component {
         }
 
         function initWeb3() {
-          const web3 = new Web3('https://public-node.testnet.rsk.co');
+          const rskEndpoint = 'https://public-node.testnet.rsk.co';
+          const web3 = new Web3(rskEndpoint);
           window.ethereum = web3;
           window.ethereum.selectedAddress = '${address}'
           window.ethereum.networkVersion = '31'
           window.web3 = web3
           window.web3.toDecimal = web3.utils.toDecimal
+          const provider = new ethers.providers.JsonRpcProvider(rskEndpoint);
           const config = {
             isEnabled: true,
             isUnlocked: true,
             networkVersion: '31',
             onboardingcomplete: true,
-            selectedAddress: '0x175E749519254AF19BF729Ff2275d9F1D0214dE5',
+            selectedAddress: '${address}',
           }
           window.ethereum.publicConfigStore = {
             _state: {
@@ -91,52 +72,64 @@ class DAppBrowser extends Component {
           window.ethereum.on = (method, callback) => { if (method) {console.log(method)} }
 
           window.ethereum.sendAsync = async (payload, callback) => {
-            const { method } = payload
-            alert(method)
-            let err, res, result
+            let err, res
             try {
-              if (method === 'eth_getBlockByNumber') {
-                result = await web3.eth.getBlock('latest')
-              } else if (method === 'eth_sendTransaction') {
+              const {method, params, jsonrpc, id} = payload
+              console.log('payload: ', payload)
+              let result = ''
+              if (method === 'eth_call') {
+                result = await provider.call(params[0], params[1])
+              }
+              if (method === 'eth_requestAccounts') {
+                result = ['${address}']
+              }
+              if (method === 'eth_estimateGas') {
+                result = await provider.estimateGas(params[0])
+                result = result.toNumber()
+              }
+              if (method === 'eth_sendTransaction') {
                 result = await getHash(payload)
                 hash = result
-              } else if (method === 'eth_getTransactionReceipt') {
-                result = await web3.eth.getTransactionReceipt(hash)
-
-                // Need set result to null, otherwise the web will show error alert
-                if (result && result.status && status) {
-                  alert('Get tRif success')
-                  status = result.status
-                  result = null
-                }
+              }
+              if (method === 'eth_getTransactionReceipt') {
+                result = await provider.getTransactionReceipt(hash)
               }
 
-              res = {
-                id,
-                jsonrpc: '2.0',
-                result,
-              }
-              id += 1
+              console.log('result: ', {id, jsonrpc, result})
+              res = {id, jsonrpc, result}
             } catch(err) {
               err = err
             }
-
             callback(err, res)
           }
 
           window.ethereum.send = window.ethereum.sendAsync
+          window.web3.setProvider(window.ethereum)
         }
 
-        const container = (document.head || document.documentElement)
-        const content = 'https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js'
-        const script = document.createElement('script')
-        script.setAttribute('type', 'text/javascript')
-        script.setAttribute('src', content)
-        script.onload = () => {
-          initWeb3()
-          script.remove()
+        let scriptCount = 0
+
+        function loadJsFile(content) {
+          const container = (document.head || document.documentElement)
+          const script = document.createElement('script')
+          script.setAttribute('type', 'text/javascript')
+          script.setAttribute('src', content)
+          script.onload = () => {
+            scriptCount += 1
+            script.remove()
+          }
+          container.insertBefore(script, container.children[0])
         }
-        container.insertBefore(script, container.children[0])
+
+        loadJsFile('https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js')
+        loadJsFile('https://storage.googleapis.com/storage-rwallet/ethers.min.js')
+
+        let timer = setInterval(() => {
+          if (scriptCount === 2) {
+            initWeb3()
+            clearInterval(timer)
+          }
+        }, 100);
       }) ();
       true
     `
@@ -147,15 +140,13 @@ class DAppBrowser extends Component {
     const url = navigation.state.params.url || '';
     const title = navigation.state.params.title || url;
 
-    const mnemonic = 'eye divorce point script garage clutch cream useless pill cheese produce brush';
+    const rskEndpoint = 'https://public-node.testnet.rsk.co';
+    // input your own 12-words mnemonic
+    const mnemonic = '';
     const provider = new ethers.providers.JsonRpcProvider(rskEndpoint);
     const mnemonicWallet = ethers.Wallet.fromMnemonic(mnemonic, "m/44'/37310'/0'/0/0");
     const wallet = new ethers.Wallet(mnemonicWallet.privateKey, provider);
-    console.log('wallet: ', wallet);
-    const contract = new ethers.Contract(contractAddress, abi, wallet);
-    console.log('contract: ', contract);
     const addr = ethers.utils.getAddress(wallet.address.toLowerCase());
-    console.log('addr: ', addr);
     const jsCode = this.getJsCode(addr);
 
     return (
@@ -163,7 +154,7 @@ class DAppBrowser extends Component {
         <OperationHeader title={title} onBackButtonPress={() => navigation.goBack()} />
         <WebView
           // source={{ uri: 'https://faucet.rifos.org/' }}
-          source={{ uri: url }}
+          source={{ uri: 'https://testnet.manager.rns.rifos.org/' }}
           // source={{ uri: 'http://localhost:3000' }}
           ref={(webview) => { this.webview = webview; }}
           javaScriptEnabled
@@ -172,17 +163,23 @@ class DAppBrowser extends Component {
           onMessage={(event) => {
             const { data } = event.nativeEvent;
             const payload = JSON.parse(data);
-            const { method } = payload;
+            const { method, params } = payload;
             if (method === 'eth_sendTransaction') {
               try {
-                callAuthVerify(() => {
-                  contract.dispense(addr).then((tx) => {
-                    console.log('tx: ', tx);
-                    this.webview.postMessage(tx.hash);
-                  }).catch((err) => {
-                    console.log('err: ', err);
-                    this.webview.postMessage('');
-                  });
+                callAuthVerify(async () => {
+                  const nonce = await provider.getTransactionCount(wallet.address);
+                  const txData = {
+                    nonce: nonce + 1,
+                    data: params[0].data,
+                    gasLimit: params[0].gas,
+                    gasPrice: params[0].gasPrice,
+                    to: params[0].to,
+                  };
+                  const signedTransaction = await wallet.sign(txData);
+                  console.log('signedTransaction: ', signedTransaction);
+                  const result = await provider.sendTransaction(signedTransaction);
+                  console.log('result: ', result);
+                  this.webview.postMessage(result.hash);
                 }, () => null);
               } catch (error) {
                 console.log(error);
