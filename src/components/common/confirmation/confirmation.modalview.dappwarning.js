233d43a6c400cd2bab32ca3eeaff06d486c9724e
AmazingBeerBelly <amazingbeerbelly@gmail.com>
2020-07-02 11:10:10 +0800

    remove close button, expand cancel click range

--- src/components/common/confirmation/confirmation.modalview.dappwarning.js
+++ src/components/common/confirmation/confirmation.modalview.dappwarning.js
@@ -3,34 +3,17 @@ import React from 'react';
 import {
   View,
   StyleSheet,
-  TouchableOpacity,
-  Image,
 } from 'react-native';
 import PropTypes from 'prop-types';
 
 import NewFeatureModalView from './confirmation.modalview.newfeature';
 
 const styles = StyleSheet.create({
-  closeBtn: {
-    marginTop: 34,
-    marginLeft: 53,
-    width: 20,
-    height: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    zIndex: 999,
-  },
-  closeImage: {
-    width: 12,
-    height: 12,
-    resizeMode: 'cover',
-  },
   titleStyle: {
     fontFamily: 'Avenir-Heavy',
     fontSize: 17,
     color: 'black',
-    marginTop: 34,
+    marginTop: 6,
   },
   messageStyle: {
     fontFamily: 'Avenir-Book',
@@ -39,15 +22,10 @@ const styles = StyleSheet.create({
   },
 });
 
-const closeImage = require('../../../assets/images/icon/close.png');
-
 const DappWarningConfirmation = ({
   title, message, onCancelPressed, onConfirmPressed,
 }) => (
   <View>
-    <TouchableOpacity style={styles.closeBtn} onPress={onCancelPressed}>
-      <Image source={closeImage} style={styles.closeImage} />
-    </TouchableOpacity>
     <NewFeatureModalView
       title={title}
       message={message}
