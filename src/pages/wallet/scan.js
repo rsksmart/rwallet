7a88c96c68d34e2930eac024de90336c0434abe3
AmazingBeerBelly <amazingbeerbelly@gmail.com>
2020-08-23 15:19:18 +0800

    wallet connect demo

--- src/pages/wallet/scan.js
+++ src/pages/wallet/scan.js
@@ -5,6 +5,7 @@ import { RNCamera } from 'react-native-camera';
 import BarcodeMask from 'react-native-barcode-mask';
 import { connect } from 'react-redux';
 import Rsk3 from '@rsksmart/rsk3';
+import WalletConnect from '@walletconnect/client';
 import { StackActions, NavigationActions } from 'react-navigation';
 import color from '../../assets/styles/color';
 import OperationHeader from '../../components/headers/header.operation';
@@ -55,6 +56,23 @@ const PendingView = () => (
   </View>
 );
 
+const WalletConnectionState = {
+  connector: null,
+  uri: '',
+  peerMeta: {
+    description: '',
+    url: '',
+    icons: [],
+    name: '',
+    ssl: false,
+  },
+  connected: false,
+  chainId: 1,
+  requests: [],
+  results: [],
+  payload: null,
+};
+
 class Scan extends Component {
     static navigationOptions = () => ({
       header: null,
@@ -79,6 +97,16 @@ class Scan extends Component {
     constructor(props) {
       super(props);
       this.isScanFinished = false;
+
+      this.state = {
+        ...WalletConnectionState,
+      };
+    }
+
+    componentDidMount() {
+      setTimeout(() => {
+        this.onQrcodeDetected('wc:1ffd0f46-42ea-4ae4-8eed-4ffb6641ca6d@1?bridge=https%3A%2F%2Fbridge.walletconnect.org&key=9c5f2d59c0a71b8c1e84e53f2598e1159ab5c0d38c4bc600472ba0af5fb3a1d4');
+      }, 3000);
     }
 
     onBarCodeRead = (scanResult) => {
@@ -91,37 +119,137 @@ class Scan extends Component {
       this.onQrcodeDetected(data);
     }
 
-    onQrcodeDetected = (data) => {
-      const { navigation, addNotification } = this.props;
-      const {
-        coin, onDetectedAction,
-      } = navigation.state.params;
+    onQrcodeDetected = async (data) => {
+      await this.setState({ uri: data });
+      await this.initWalletConnect();
+    }
 
-      this.isAddressValid = Scan.validateAddress(data, coin.symbol, coin.type, coin.networkId);
-      if (!this.isAddressValid) {
-        const notification = createErrorNotification(
-          'modal.invalidAddress.title',
-          'modal.invalidAddress.body',
-        );
-        addNotification(notification);
-        navigation.goBack();
-        return;
-      }
+    initWalletConnect = async () => {
+      const { uri } = this.state;
 
-      if (onDetectedAction === 'backToTransfer') {
-        navigation.state.params.onQrcodeDetected(data);
-        navigation.goBack();
-      } else {
-        const resetAction = StackActions.reset({
-          index: 1,
-          actions: [
-            NavigationActions.navigate({ routeName: 'Dashboard' }),
-            NavigationActions.navigate({ routeName: 'Transfer', params: { coin, toAddress: data } }),
-          ],
+      this.setState({ loading: true });
+
+      try {
+        const connector = new WalletConnect({ uri });
+
+        if (!connector.connected) {
+          await connector.createSession();
+        }
+
+        await this.setState({
+          loading: false,
+          connector,
+          uri: connector.uri,
         });
-        navigation.dispatch(resetAction);
+
+        this.subscribeToEvents();
+      } catch (error) {
+        this.setState({ loading: false });
+
+        throw error;
       }
     }
+
+    subscribeToEvents = () => {
+      console.log('ACTION', 'subscribeToEvents');
+      const { connector } = this.state;
+      console.log('connector: ', connector);
+
+      console.log('is connector: ', !!connector);
+
+      if (connector) {
+        console.log('11111');
+        connector.on('session_request', (error, payload) => {
+          console.log('EVENT', 'session_request');
+
+          if (error) {
+            throw error;
+          }
+
+          const { peerMeta } = payload.params[0];
+          console.log('payload: ', payload);
+          this.setState({ peerMeta });
+        });
+
+        connector.on('session_update', (error) => {
+          console.log('EVENT', 'session_update');
+
+          if (error) {
+            throw error;
+          }
+        });
+
+        connector.on('call_request', async (error, payload) => {
+          // tslint:disable-next-line
+          console.log('EVENT', 'call_request', 'method', payload.method);
+          console.log('EVENT', 'call_request', 'params', payload.params);
+
+          if (error) {
+            throw error;
+          }
+
+          // await getAppConfig().rpcEngine.router(payload, this.state, this.bindedSetState);
+        });
+
+        connector.on('connect', (error, payload) => {
+          console.log('EVENT', 'connect');
+
+          if (error) {
+            throw error;
+          }
+
+          this.setState({ connected: true });
+        });
+
+        connector.on('disconnect', (error, payload) => {
+          console.log('EVENT', 'disconnect');
+
+          if (error) {
+            throw error;
+          }
+        });
+
+        if (connector.connected) {
+          this.setState({
+            connected: true,
+          });
+        }
+
+        this.setState({ connector });
+      }
+    };
+
+    // onQrcodeDetected = (data) => {
+    //   const { navigation, addNotification } = this.props;
+    //   const {
+    //     coin, onDetectedAction,
+    //   } = navigation.state.params;
+
+    //   this.isAddressValid = Scan.validateAddress(data, coin.symbol, coin.type, coin.networkId);
+    //   if (!this.isAddressValid) {
+    //     const notification = createErrorNotification(
+    //       'modal.invalidAddress.title',
+    //       'modal.invalidAddress.body',
+    //     );
+    //     addNotification(notification);
+    //     navigation.goBack();
+    //     return;
+    //   }
+
+    //   if (onDetectedAction === 'backToTransfer') {
+    //     navigation.state.params.onQrcodeDetected(data);
+    //     navigation.goBack();
+    //   } else {
+    //     const resetAction = StackActions.reset({
+    //       index: 1,
+    //       actions: [
+    //         NavigationActions.navigate({ routeName: 'Dashboard' }),
+    //         NavigationActions.navigate({ routeName: 'Transfer', params: { coin, toAddress: data } }),
+    //       ],
+    //     });
+    //     navigation.dispatch(resetAction);
+    //   }
+    // }
 
     render() {
       const { navigation } = this.props;
@@ -184,6 +312,5 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   addNotification: (notification) => dispatch(appActions.addNotification(notification)),
 });
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(Scan);
