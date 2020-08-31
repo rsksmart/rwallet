ee8d76c9dd99acb272256637ac2aa7400a91a0ad
imstar15 <imstar15@qq.com>
2020-08-31 21:29:20 +0800

    Enter operation page when there's only one asset

--- src/pages/wallet/dashboard/list.js
+++ src/pages/wallet/dashboard/list.js
@@ -171,8 +171,13 @@ class WalletList extends Component {
 
   onSendPressed = (wallet) => {
     const { navigation, addNotification } = this.props;
+    const { coins } = wallet;
     if (wallet.walletType === WalletType.Readonly) {
       addNotification(createReadOnlyLimitNotification());
+      return;
+    }
+    if (coins.length === 1) {
+      navigation.navigate('Transfer', { wallet, coin: coins[0] });
       return;
     }
     navigation.navigate('SelectWallet', { operation: 'send', wallet });
@@ -180,13 +185,23 @@ class WalletList extends Component {
 
   onReceivePressed = (wallet) => {
     const { navigation } = this.props;
+    const { coins } = wallet;
+    if (coins.length === 1) {
+      navigation.navigate('WalletReceive', { coin: coins[0] });
+      return;
+    }
     navigation.navigate('SelectWallet', { operation: 'receive', wallet });
   }
 
   onScanQrcodePressed = (wallet) => {
     const { navigation, addNotification } = this.props;
+    const { coins } = wallet;
     if (wallet.walletType === WalletType.Readonly) {
       addNotification(createReadOnlyLimitNotification());
+      return;
+    }
+    if (coins.length === 1) {
+      navigation.navigate('Scan', { coin: coins[0], onDetectedAction: 'navigateToTransfer' });
       return;
     }
     navigation.navigate('SelectWallet', {
