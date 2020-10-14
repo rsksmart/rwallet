498fc32e6f6f621e58877cf2bd1107e6439ac7c6
AmazingBeerBelly <amazingbeerbelly@gmail.com>
2020-10-14 16:21:09 +0800

    disable eslint to shim.js

--- shim.js
+++ shim.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable global-require */
+/* eslint-disable no-underscore-dangle */
 if (typeof __dirname === 'undefined') global.__dirname = '/';
 if (typeof __filename === 'undefined') global.__filename = '';
 if (typeof process === 'undefined') {
