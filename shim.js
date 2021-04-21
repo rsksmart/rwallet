32e4d70acb7c11ea8b628bbcc71f91a509cdd3d8
Patricio <patricio@iovlabs.org>
2021-04-21 12:20:50 -0300

    fix: android bigint issue

--- shim.js
+++ shim.js
@@ -17,7 +17,7 @@ if (typeof process === 'undefined') {
 
 process.browser = false;
 if (typeof Buffer === 'undefined') global.Buffer = require('buffer').Buffer;
-
+if (typeof BigInt === 'undefined') global.BigInt = require('big-integer');
 // global.location = global.location || { port: 80 }
 const isDev = typeof __DEV__ === 'boolean' && __DEV__;
 process.env.NODE_ENV = isDev ? 'development' : 'production';
