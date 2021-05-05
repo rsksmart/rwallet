c970d1a8a8293fe2ddb579e15a47263a231658c0
jessgusclark <hello@developerjesse.com>
2021-05-05 16:09:36 +0300

    Add comments and allow for additionalInfo.

--- src/common/error/report.error.js
+++ src/common/error/report.error.js
@@ -2,19 +2,31 @@ import { getSystemName } from 'react-native-device-info';
 import VersionNumber from 'react-native-version-number';
 import ParseHelper from '../parse';
 
-const mockSend = (data) => {
+/**
+ * Send the data object to the server. Mocked right now and will be replaced in the future
+ * @param {Object} data object that is sent
+ */
+const sendToServer = (data) => {
   console.log('@jesse, data  :', data);
   console.log('@jesse, string:', JSON.stringify(data));
-  Promise.resolve(true);
+  return Promise.resolve(true);
 };
 
-const ReportErrorToServer = ({ developerComment, errorObject }) => ParseHelper.getUser().then((parseUser) => {
+/**
+ * Creates a report in JSON format and sends it to the server to be saved
+ * @param {object} data
+ * @param {Object} data.developerComment {string} a comment added by a developer for extra context
+ * @param {Object} data.errorObject {Error} the error that was thrown
+ * @param {Object} data.additionalInfo {Object} an optional JSON object that can provide extra information
+ */
+const ReportErrorToServer = ({ developerComment, errorObject, additionalInfo }) => ParseHelper.getUser().then((parseUser) => {
   // create the object to be sent
   const toServerObject = {
     comment: developerComment || '',
+    additionalInfo: additionalInfo ? JSON.stringify(additionalInfo) : '',
     errorObject: {
-      message: errorObject.message,
       code: errorObject.code,
+      message: errorObject.message,
       stack: errorObject.stack.toString(),
     },
     userId: parseUser.id,
@@ -24,7 +36,8 @@ const ReportErrorToServer = ({ developerComment, errorObject }) => ParseHelper.g
   };
 
   // send object to server
-  mockSend(toServerObject);
+  sendToServer(toServerObject)
+    .catch((error) => console.log(error));
 });
 
 export default ReportErrorToServer;
