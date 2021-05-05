2efa915b8cfb87175d5c11aa52aef4049dfcaf36
jessgusclark <hello@developerjesse.com>
2021-05-05 15:24:01 +0300

    Convert error to JSON object.

--- src/common/error/report.error.js
+++ src/common/error/report.error.js
@@ -3,14 +3,20 @@ import VersionNumber from 'react-native-version-number';
 import ParseHelper from '../parse';
 
 const mockSend = (data) => {
-  console.log('@jesse', data);
+  console.log('@jesse, data  :', data);
+  console.log('@jesse, string:', JSON.stringify(data));
   Promise.resolve(true);
 };
 
 const ReportErrorToServer = ({ developerComment, errorObject }) => ParseHelper.getUser().then((parseUser) => {
+  // create the object to be sent
   const toServerObject = {
     comment: developerComment || '',
-    errorObject: errorObject.toString(),
+    errorObject: {
+      message: errorObject.message,
+      code: errorObject.code,
+      stack: errorObject.stack.toString(),
+    },
     userId: parseUser.id,
     username: parseUser.getUsername(),
     platform: getSystemName(),
