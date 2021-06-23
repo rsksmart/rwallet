6465262affd6a87241cf950b8a4f9913ccab2418
Patricio <patricio@iovlabs.org>
2021-06-23 17:42:41 -0300

    fix: fix dependencies and imports

--- src/navigation/stack.mine.js
+++ src/navigation/stack.mine.js
@@ -1,4 +1,5 @@
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import { defaultNavigationOptions, routeConfigMaps } from '../common/navigation.config';
 
